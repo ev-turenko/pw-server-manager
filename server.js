45098c5a7d85bdd8bc5a0ef995810ec5f7c71f27
Evgenii T <johnnitzevans@yahoo.com>
2025-06-29 00:41:49 -0600

    port updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { chromium } from 'playwright';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3011;
 
 app.use(express.json());
 
